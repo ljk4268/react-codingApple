91a14e02eecfb5ef1be860489581a53368c09300
ljk4268 <ljk4268@kakao.com>
2022-05-17 16:42:04 +0900

    애니메이션작업까지

--- App.js
+++ App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Container, Nav} from 'react-bootstrap';
+import bg from './bg.png';
+import data from './Data';
+import {Routes, Route, useNavigate, Outlet, Link} from 'react-router-dom'
+import Detail from './pages/Detail';
+import axios from 'axios';
+
 
 function App() {
+
+  let [shoes, setShose] = useState(data)
+  let navigate = useNavigate();
+  let [count, setCount] = useState(0);
+
+  function Click(){
+    
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link onClick={()=>{ navigate('/') }}>Home</Nav.Link>
+          <Nav.Link onClick={()=>{ navigate('/detail') }}>Cart</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path='/' element={
+          <>
+            <div className="main-bg" style={{ backgroundImage : 'url(' + bg + ')'}}></div>
+
+            <div className="container">
+              <div className="row">
+                {
+                  shoes.map(function(a, i){
+                    return(
+                        <Card  shoes={shoes[i]} i={i} key={i} />
+                    )
+                  })
+                }
+              </div> 
+            </div>
+            <button onClick={()=>{
+              setCount(count += 1);
+
+              if(count == 1){
+                axios.get('https://codingapple1.github.io/shop/data2.json')
+                .then((결과)=>{ 
+                  let more = [...shoes]
+                  let moreShose = more.concat(결과.data)
+                  setShose(moreShose);
+                })
+                .catch((error)=>{ console.log(error) }); 
+              } else if ( count == 2) {
+                axios.get('https://codingapple1.github.io/shop/data3.json')
+                .then((결과)=>{ 
+                  let more1 = [...shoes]
+                  let moreShose1 = more1.concat(결과.data)
+                  setShose(moreShose1);
+                })
+                .catch((error)=>{ console.log(error) });
+              } else if ( count ==3 ){
+                alert('더 이상 상품이 없습니다. ')
+              }
+
+              }}>더보기{count}/2</button>
+
+              
+          </>
+        }/>
+    
+    <Route path='/detail/:id' element={ <Detail shoes={shoes}/> }/>
+
+    <Route path='*' element={<div>없는 페이지에요.</div>}/>   
+
+      </Routes>
+
+
     </div>
   );
+}
+
+
+function Card(props){
+  return(
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i+1) +'.jpg'} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </div>
+  )
 }
 
 export default App;
---
+++ pages/Detail.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import { useParams, } from "react-router-dom";
+import {Nav} from 'react-bootstrap';
+
+
+
+function Detail(props){
+
+  let {id} = useParams();
+  let 찾은상품 = props.shoes.find((x) => x.id == id );
+  let [popUp, setPopUp] = useState(true);
+  let [result, setResult] = useState('');
+  let [탭, 탭변경] = useState(0);
+  let [fade2, setFade2] = useState('');
+
+  function CheckNum(e){
+    setResult(e.target.value)
+    if(isNaN(result) == true){
+      alert('경고창')
+      e.target.value = ''
+      e.target.focous()
+    }
+  }
+
+  useEffect(()=>{
+    let a = setTimeout(()=>{ setFade2('end') },100)
+    
+    return()=>{
+      clearTimeout(a)
+      setFade2('')
+    }
+  },[])
+
+  return(
+    <div className={'container start' + fade2}>
+      
+      <div className="row">
+        <div className="col-md-6">
+          <img src={"https://codingapple1.github.io/shop/shoes" + `${Number(id) + 1}` + ".jpg" }width="100%" />
+        </div>
+        <div className="col-md-6">
+        <input type="text" onChange={(e)=>{CheckNum(e)}}></input>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
+          <button className="btn btn-danger">주문하기</button> 
+        </div>
+      </div>
+
+        <Nav variant="tabs"  defaultActiveKey="link0">
+          <Nav.Item>
+            <Nav.Link onClick={()=>{탭변경(0)}} eventKey="link0">버튼0</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={()=>{탭변경(1)}} eventKey="link1">버튼1</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={()=>{탭변경(2)}} eventKey="link2">버튼2</Nav.Link>
+          </Nav.Item>
+        </Nav> 
+
+        <TabContent 탭={탭}/>
+
+    </div> 
+  )
+}
+
+function TabContent({탭}){
+  
+  let [fade, setFade] = useState('')
+
+  useEffect(()=>{
+    let a = setTimeout(()=>{ setFade('end') },100)
+    
+    return()=>{
+      clearTimeout(a)
+      setFade('')
+    }
+  },[탭])
+
+  return ( <div className={'start ' + fade}>
+    {[<div>내용0</div>, <div>내용1</div>,<div>내용2</div> ][탭]}
+  </div> )
+  
+}
+
+export default Detail
