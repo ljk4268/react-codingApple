0fb6a44143524c91921f7eceae938bccacedfc9c
ljk4268 <ljk4268@kakao.com>
2022-05-17 18:33:52 +0900

    모듈없앤버전

---
+++ src/pages/Cart.js
@@ -0,0 +1,48 @@
+import {Table} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName, changeCount } from './../store'
+
+function Cart(){
+
+  let state = useSelector((state)=> state )
+  let dispatch = useDispatch()
+
+  let cart = state.cart
+
+  return(
+    <div>
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>상품명</th>
+            <th>수량</th>
+            <th>변경하기</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {
+            cart.map(function(a, i){
+              return(
+                <tr key={i}>
+                  <td>{i+1}</td>
+                  <td>{cart[i].name}</td>
+                  <td>{cart[i].count}</td>
+                  <td>
+                    <button onClick={()=>{
+                      dispatch(changeCount(i))
+                    }}>+</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+          
+        </tbody>
+      </Table> 
+    </div>
+  )
+}
+
+export default Cart
